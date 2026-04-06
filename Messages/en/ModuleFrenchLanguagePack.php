675448ab3d828d04e3a92196ea53980917502707
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:04 +0700

    Initial commit: ModuleFrenchLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleFrenchLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleFrenchLanguagePack' => 'French Language Pack',
+    'SubHeaderModuleFrenchLanguagePack' => 'Complete French language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleFrenchLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleFrenchLanguagePack' => 'Языковой пакет - Французский',
+    'SubHeaderModuleFrenchLanguagePack' => 'Комплексная поддержка французского языка для системы MikoPBX',
+];
